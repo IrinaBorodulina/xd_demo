5dac8b9292ae5231f27089d2e5bfd076adbb5b4f
Irina Borodulina <irina.borodulina@eltex.loc>
2023-06-13 10:37:43 +0700

    liquibase

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS reservations;
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users
-(
-    id INT,
-    CONSTRAINT pk_users_id PRIMARY KEY (id)
-);
-CREATE TABLE reservations
-(
-    id      INT,
-    user_id INT NOT NULL,
-    info    VARCHAR,
-    CONSTRAINT fk_users_id
-        FOREIGN KEY (user_id)
-            REFERENCES users (id)
-);
+-- DROP TABLE IF EXISTS reservations;
+-- DROP TABLE IF EXISTS users;
+--
+-- CREATE TABLE users
+-- (
+--     id INT,
+--     CONSTRAINT pk_users_id PRIMARY KEY (id)
+-- );
+-- CREATE TABLE reservations
+-- (
+--     id      INT,
+--     user_id INT NOT NULL,
+--     info    VARCHAR,
+--     CONSTRAINT fk_users_id
+--         FOREIGN KEY (user_id)
+--             REFERENCES users (id)
+-- );
