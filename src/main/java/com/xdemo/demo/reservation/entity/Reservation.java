408e91144b5223c749a68d6461cf357a96f72711
Irina Borodulina <irina.borodulina@eltex.loc>
2023-06-14 11:04:03 +0700

    liquibase migration and lombok

--- src/main/java/com/xdemo/demo/reservation/entity/Reservation.java
+++ src/main/java/com/xdemo/demo/reservation/entity/Reservation.java
@@ -3,31 +3,28 @@ package com.xdemo.demo.reservation.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.xdemo.demo.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "reservations")
+@Getter
+@Setter
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "rsv_seq")
     @SequenceGenerator(name = "rsv_seq", sequenceName = "rsv_seq", allocationSize = 1)
     @Column(name = "id")
+    @JsonProperty
     private Integer id;
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
     @Column
+    @JsonProperty
     private String info;
-
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getId() {
-        return id;
-    }
 
     @JsonIgnore
     public User getUser() {
@@ -39,12 +36,5 @@ public class Reservation {
         this.user = user;
     }
 
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
-    }
 }
 
--- src/main/java/com/xdemo/demo/user/entity/User.java
+++ src/main/java/com/xdemo/demo/user/entity/User.java
@@ -3,6 +3,8 @@ package com.xdemo.demo.user.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.xdemo.demo.reservation.entity.Reservation;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -10,23 +12,22 @@ import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "usr_seq")
     @SequenceGenerator(name = "usr_seq", sequenceName = "usr_seq", allocationSize = 1)
     @Column(name = "id")
+    @JsonProperty
     private Integer id;
+
+    @Column(name = "birth_year")
+    @JsonProperty
+    private Integer birthYear;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Reservation> reservations = new ArrayList<>();
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getId() {
-        return id;
-    }
 
     @JsonProperty
     public List<Reservation> getReservations() {
